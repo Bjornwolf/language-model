c2df957d4c1b734e6a3f8b66fd9314ed48560c35
Filip Chudy <i246059@lab110-10.stud.ii>
2015-11-25 13:31:53 +0100

    file size 300, overlap 100, unicode, working now

--- utils/unicode_cutter.py
+++ utils/unicode_cutter.py
@@ -1,6 +1,6 @@
 import os
 import pickle
-import codecs.open as open
+import codecs
 
 files_size = 300
 overlap_size = 100
@@ -9,7 +9,7 @@ def build_set(name, fnames, prefix_path, out_path):
     print name
     for fname in fnames:
         full_path = prefix_path + fname
-        with open(full_path, 'r') as f:
+        with codecs.open(full_path, 'r') as f:
             conc = f.read()
         piece_no = 1
         i = int(fname[3:]) # bardzo brzydkie wykorzystanie formatu 'artXXXX'
@@ -19,18 +19,18 @@ def build_set(name, fnames, prefix_path, out_path):
                 os.mkdir(out_path + str(piece_no), 0755)
             except:
                 pass
-            with open(out_path + str(piece_no) + '/' + fname, 'w+') as g:
-                g.write(conc[:files_size])
+            with open(out_path + str(piece_no) + '/' + fname, 'wb+') as g:
+               g.write(conc[:files_size])
             conc = conc[files_size - overlap_size:]
             piece_no += 1
             
-prefix_path = '../data/plwiki_raw/plwiki/'
+prefix_path = '/pio/lscratch/1/i246059/language-model/data/plwiki_raw/plwiki/'
 
 
-train_fnames = pickle.load(open('../data/plwiki_raw/train_set_names', 'rb'))
-out_path = '../data/plwiki_1gu/train/'
+train_fnames = pickle.load(open('/pio/lscratch/1/i246059/language-model/data/plwiki_raw/train_set_names', 'rb'))
+out_path = '/pio/lscratch/1/i246059/language-model/data/plwiki_1gu/train/'
 build_set("TRAIN", train_fnames, prefix_path, out_path)
 
-test_fnames = pickle.load(open('../data/plwiki_raw/test_set_names', 'rb'))
-out_path = '../data/plwiki_1gu/test/'
+test_fnames = pickle.load(open('/pio/lscratch/1/i246059/language-model/data/plwiki_raw/test_set_names', 'rb'))
+out_path = '/pio/lscratch/1/i246059/language-model/data/plwiki_1gu/test/'
 build_set("TEST", test_fnames, prefix_path, out_path)
